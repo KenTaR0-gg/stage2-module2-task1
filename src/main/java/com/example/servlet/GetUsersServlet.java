bc3a3afa2d2d44a75d38dbade996f5714af8fca9
KenTaR0-gg <kotya.piso@gmail.com>
2026-09-16 12:10:34 +0300

    stage2_Servlet_1

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -15,6 +15,6 @@ public class GetUsersServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setAttribute("users", Warehouse.getInstance().getUsers());
-        req.getRequestDispatcher("users.jsp").forward(req, resp);
+        req.getRequestDispatcher("/views/users.jsp").forward(req, resp);
     }
 }
